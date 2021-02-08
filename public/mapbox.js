3fd5e9af6ac5684d69ea410288cb3a61491caed4
IanMcbull <ianmugenya@gmail.com>
2021-02-08 07:50:27 +0300

    v1.2

--- public/mapbox.js
+++ public/mapbox.js
@@ -26,7 +26,7 @@ fetch(`/weather?location=${location}`).then(res=>{
     card_header.innerHTML = data.Location;
     weather.innerHTML = `<i class="fas fa-smog fa-2x"></i>  Forecast: ${data.forecast}`;
     timezone.innerHTML = `<i class="far fa-clock fa-2x"></i> Timezone: ${data.timezone}`;
-    temp.innerHTML = `<i class="fas fa-temperature-low fa-2x"></i> Temperature: ${data.temp} Celsius`;
+    temp.innerHTML = `<i class="fas fa-temperature-low fa-2x"></i> Temperature: ${data.temp} Degrees Celsius`;
     humidity.innerHTML = `<i class="fas fa-thermometer-full fa-2x"></i> Humidity: ${data.humidity}`
 
     card.style.visibility = 'visible';
